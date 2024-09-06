b83d643d0e7cf15bb4e326e8a25ffa65a88820e8
Ollitas <tudtfkcjbgu05462@gmail.com>
2024-09-05 21:42:59 -0600

    version bien

--- Programa funciones/app.js
+++ Programa funciones/app.js
@@ -3,14 +3,15 @@ let numeroVidas = 3;
 numeroRandom = Math.floor(Math.random()*(10-1)+1);
 console.log(numeroRandom);
 
+let valor1 = parseInt(prompt(`Ingresa un numero del 1 al 10`));
+
 comparacion();
 
 function comparacion(){
-    valor1 = parseInt(prompt(`Ingresa un numero del 1 al 10`));
-    while (numeroRandom !== valor1 && numeroVidas >1){
+    while (numeroRandom !== valor1 && numeroVidas>1){
         numeroVidas--;
         alert(`Fallaste, tu numero de vidas es ${numeroVidas}`);
-        prompt(`Ingresa un numero del 1 al 10: `);  
+        valor1 = parseInt(prompt(`Ingresa un numero del 1 al 10: `));  
     }
     verdades();
 }
